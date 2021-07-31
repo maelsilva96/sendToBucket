0c49c492b1d2d9dc1b60300e97e7d10522995d70
Ismael Souza <ismael.souza@equals.com.br>
2021-07-31 12:43:04 -0300

    Teste 1

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ try {
     const request = https.get(urlFile, function(response) {
       response.pipe(file);
     });
-    exec.exec('./vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe').then((item) => {
-        console.log(item);
-    }).catch((err) => {
-        console.log(err);
-    });
+    const result = async () => 
+        await exec.exec('./vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe');
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
