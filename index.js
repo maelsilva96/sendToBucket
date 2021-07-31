08fb518a401c2fd60aaf07310f9539df575ec7d0
Ismael Souza <ismael.souza@equals.com.br>
2021-07-31 11:51:15 -0300

    Teste

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const https = require('https');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('urlFile');
-    console.log(`Hello ${nameToGreet}!`);
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet2 = core.getInput('outputFile');
-    console.log(`Hello ${nameToGreet2}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const urlFile = core.getInput('urlFile');
+    const outputFile = core.getInput('outputFile');
+    const file = fs.createWriteStream(outputFile);
+    const request = https.get(urlFile, function(response) {
+      response.pipe(file);
+    });
+    
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
