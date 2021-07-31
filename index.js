bb441fd067438aca393b39e9345582817fabef9e
Ismael Souza <ismael.souza@equals.com.br>
2021-07-31 11:48:00 -0300

    Teste

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+    const nameToGreet = core.getInput('urlFile');
     console.log(`Hello ${nameToGreet}!`);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet2 = core.getInput('outputFile');
+    console.log(`Hello ${nameToGreet2}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
