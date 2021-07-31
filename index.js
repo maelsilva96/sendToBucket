a3cfee6115a060607e95b1226581243007bbc407
Ismael Souza <ismael.souza@equals.com.br>
2021-07-31 11:52:21 -0300

    Teste

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const https = require('https');
+const fs = require('fs');
 
 try {
     const urlFile = core.getInput('urlFile');
@@ -9,7 +10,7 @@ try {
     const request = https.get(urlFile, function(response) {
       response.pipe(file);
     });
-    
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
